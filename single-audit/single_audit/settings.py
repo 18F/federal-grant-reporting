65587959cf6763ea585600676e978b3b461f7565
Fureigh <rhys.fureigh@gsa.gov>
2019-03-19 19:22:26 -0700

    Add designstandards and localflavor
    * https://github.com/department-of-veterans-affairs/django-designstandards
    * Installing localflavor primarily for its States select field.
    
    @todo: Revisit whether to use the django-designstandards package.

--- single-audit/single_audit/settings.py
+++ single-audit/single_audit/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'designstandards',
+    'localflavor',
     'fac.apps.FacConfig',
 ]
 

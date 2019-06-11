803fd03d9c6c088bd03ad6dd658852ac569c03cc
Alex Soble <alex.soble@gsa.gov>
2019-06-11 14:23:41 -0500

    Remove designstandards library from single-audit

--- single-audit/single_audit/settings/base.py
+++ single-audit/single_audit/settings/base.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'designstandards',
     'localflavor',
     'fac.apps.FacConfig',
     'distiller.apps.DistillerConfig',

011e0056b6df8bdf8f33e0c101d6e4b7c2407fa4
Alex Soble <alex.soble@gsa.gov>
2019-06-12 11:00:10 -0500

    Separate out body from base.html; attempt to add fgr_custom.css to static css

--- single-audit/resolve_findings/views.py
+++ single-audit/resolve_findings/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def finding_resolution_page(request):
-    return render(request, 'resolve_findings/base.html')
+    return render(request, 'resolve_findings/finding-resolution-page.html')

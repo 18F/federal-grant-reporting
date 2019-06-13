3accd6f7f7dadf67666c7fa90f6e50825c5fe75b
Fureigh <rhys.fureigh@gsa.gov>
2019-06-12 18:45:23 -0700

    Add Agency, Grant and Grantee models
    This commit does the following:
    * Adds Agency, Grant and Grantee models
    * Adds a few more fields to the Finding model
    * Adds a basic validator for CFDA numbers (to verify that they're
      five digits long)
    * Removes some comments
    
    https://github.com/18F/federal-grant-reporting/issues/79

--- single-audit/resolve_findings/models.py
+++ single-audit/resolve_findings/models.py
@@ -1,18 +1,54 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
-# @todo:
-#   - Tweak STATUS_TYPE_CHOICES and FINDING_TYPE_CHOICES.
-#   - Think through how to handle 'assignee'; what'll you do when the findings
-#     have been resolved? Just have it okay for this to be null/unassigned? Or
-#     have the resolved findings be 'assigned' to someone?
-#         - Also: should 'status' + 'assignee' + 'due date of next action' be
-#           bundled together?
-#   - Think through how best to incorporate 'watchers'. (Not yet represented.)
-#   - Decide how to handle/generate URLs. (Which fields would it make sense to
-#     string together without getting too verbose?)
+def validate_cfda_number(number):
+    # CFDA stands for "Catalog of Federal Domestic Assistance."
+    if len(str(number)) != 5:
+        raise ValidationError('A CFDA number must have five digits. The number \
+                               you provided is %s.' % number)
+    # @todo: Determine and add remaining kinds of CFDA number validation, when
+    #        appropriate.
+
+
+class Agency(models.Model):
+    name = models.CharField(max_length=250)
+    grantees = models.ManyToManyField(Grantee)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Grant(models.Model):
+    name = models.CharField(max_length=250)
+    cfda = models.IntegerField(
+        validators=[validate_cfda_number]
+    )
+    recipient = models.ManyToManyField(Grantee)
+
+    class Meta:
+        ordering = ('cfda',)
+
+    def __str__(self):
+        return self.name
+
+
+class Grantee(models.Model):
+    name = models.CharField(max_length=250)
+    grantors = models.ManyToManyField(Agency)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
 class Finding(models.Model):
     STATUS_TYPE_CHOICES = (
         ('new', 'New'),
@@ -21,7 +57,6 @@ class Finding(models.Model):
     )
     FINDING_TYPE_CHOICES = (
         ('material_weakness', 'Material Weakness'),
-        ('noncompliance', 'Noncompliance'),
         ('significant_deficiency', 'Significant Deficiency'),
     )
     name = models.CharField(max_length=250)
@@ -36,36 +71,9 @@ class Finding(models.Model):
     status = models.CharField(max_length=35,
                               choices=STATUS_TYPE_CHOICES,
                               default='new')
-
-    # @todo: Add the remainder!
-    #
-    # grantee = models.ForeignKey(Grantee, related_name='findings')
-    # agencies_affected = models.ManyToManyField(Agency)
-    # assignee = models.ForeignKey(User, related_name='active_findings')
-    #
-    # due_date_for_next_action
-    # due_date_for_resolution (? is that a real thing?)
-    # prior_year_findings  # @todo: Find out whether "related findings" is
-    #                      # A Thing, outside of prior year findings. Or leave
-    #                      # that question, for the moment, and focus on what
-    #                      # you already know they need. Which is: prior year.
-    #
-    # corrective action plan (i.e., next action?)
-    #   - what
-    #   - by whom
-    #   - by when
-    # corrective_action_plan = models.TextField()
-    # corrective_action_owner = models.ForeignKey(User, related_name='corrective_actions')
-    # corrective_action_due_date = models.DateTimeField()
-    #
-    # comments
-    #
-    # grant [associated with this finding]? -- check how this is currently
-    # represented.
-    #
-    # (@todo: Decide whether to include fiscal year or derive it from the
-    # finding number. Might be easier to pull findings by fiscal year if `fiscal
-    # year` is its own field, but hey.)
+    assignee = models.ForeignKey(User, related_name='findings')
+    grantee = models.ForeignKey(Grantee, related_name='findings')
+    agencies_affected = models.ManyToManyField(Agency)
 
     class Meta:
         ordering = ('-status',)

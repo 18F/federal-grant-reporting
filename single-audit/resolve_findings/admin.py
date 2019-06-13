d89910dfd12117caae4d0f86c766475f9b507b46
Fureigh <rhys.fureigh@gsa.gov>
2019-06-13 14:06:15 -0700

    Register Agency, Grant and Grantee models in the admin site
    https://github.com/18F/federal-grant-reporting/issues/79

--- single-audit/resolve_findings/admin.py
+++ single-audit/resolve_findings/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Finding
+from .models import Finding, Agency, Grant, Grantee
 
+admin.site.register(Agency)
 admin.site.register(Finding)
+admin.site.register(Grant)
+admin.site.register(Grantee)

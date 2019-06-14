6a870ebd49f3e828dc407e11294c2fbd94d2657c
Fureigh <rhys.fureigh@gsa.gov>
2019-06-14 10:23:20 -0700

    Register UserAdmin
    For further context, see https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#substituting-a-custom-user-model.
    
    https://github.com/18F/federal-grant-reporting/issues/79

--- single-audit/resolve_findings/admin.py
+++ single-audit/resolve_findings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Finding, Agency, Grant, Grantee, User
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Agency)
 admin.site.register(Finding)
 admin.site.register(Grant)

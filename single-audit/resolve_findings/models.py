d203bbab38c32f180ce111654f75e6df155ca3ee
Fureigh <rhys.fureigh@gsa.gov>
2019-06-21 22:09:16 -0700

    Add Comment model to admin interface
    https://github.com/18F/federal-grant-reporting/issues/110

--- single-audit/resolve_findings/models.py
+++ single-audit/resolve_findings/models.py
@@ -131,4 +131,4 @@ class Comment(models.Model):
         ordering = ('created',)
 
     def __str__(self):
-        return 'Comment by {} on {}'.format(self.author.name, self.finding)
+        return 'Comment by {} on {}'.format(self.author, self.finding)

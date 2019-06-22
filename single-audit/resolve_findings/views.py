5d08d82307a2602b4d826766fd107655b4eb2b39
Fureigh <rhys.fureigh@gsa.gov>
2019-06-21 22:54:38 -0700

    Add comments and comment form to finding resolution page view
    https://github.com/18F/federal-grant-reporting/issues/110

--- single-audit/resolve_findings/views.py
+++ single-audit/resolve_findings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Finding
+from .models import Finding, Comment
+from .forms import CommentForm
 
 
 def findings_list(request):
@@ -11,7 +12,24 @@ def findings_list(request):
 
 def finding_resolution_page(request, finding_id):
     finding = get_object_or_404(Finding, id=finding_id)
+
+    comments = finding.comments.filter(is_published=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.finding = finding
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     context = {
-        'finding': finding
+        'finding': finding,
+        'comments': comments,
+        'comment': new_comment,
+        'comment_form': comment_form,
     }
     return render(request, 'resolve_findings/finding-resolution-page.html', context)

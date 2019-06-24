7ea38832e826aa30d1ed96e02cc162502b903f87
Alex Soble <alex.soble@gsa.gov>
2019-06-24 15:49:26 -0500

    Specify custom textarea and classes

--- single-audit/resolve_findings/forms.py
+++ single-audit/resolve_findings/forms.py
@@ -8,3 +8,7 @@ class CommentForm(forms.ModelForm):
         # There's no need for the user to edit any of the other fields.
         # The trailing comma is required for this to be recognized as a tuple.
         fields = ('body',)
+
+        widgets = {
+            'body': forms.Textarea(attrs={'class': 'big_textarea width-full margin-top-1'}),
+        }

5a0bcd65898b2dc198cabeb8191e4d8e202f11fb
Fureigh <rhys.fureigh@gsa.gov>
2019-08-22 15:31:04 -0700

    Remove non-Distiller URLs and tests
    https://github.com/18F/federal-grant-reporting/issues/87

--- single-audit/single_audit/urls.py
+++ single-audit/single_audit/urls.py
@@ -13,20 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-# from fac import views  # @todo: Revisit, split out by app.
-from fac.views import get_single_audit_package
 from distiller import views as distiller_views
-from resolve_findings import views as findings_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('fac', get_single_audit_package),
     path('', distiller_views.prompt_for_agency_name),
     path('get-single-audits-by-agency/', distiller_views.show_agency_level_summary, name='show_relevant_audits'),
     path('generate-a-csv/', distiller_views.offer_download_of_agency_specific_csv, name='prompt_to_save_csv'),
     path('download-single-audits/', distiller_views.download_pdfs_from_fac, name='download_single_audit_pdfs'),
-    path('findings/', findings_views.findings_list, name='findings_overview'),
-    path('finding/<int:finding_id>/', findings_views.finding_resolution_page, name='finding'),
 ]

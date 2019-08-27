a2c5264b5d91a9f0440ec66ff75c95618b2115e5
Fureigh <rhys.fureigh@gsa.gov>
2019-08-27 13:08:59 -0700

    Update static asset loading in light of new BASE_DIR
    https://www.github.com/18f/federal-grant-reporting/issues/154

--- single-audit/single_audit/settings/base.py
+++ single-audit/single_audit/settings/base.py
@@ -102,11 +102,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'single_audit/staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'single_audit/static'),
     '/var/www/static/',
 ]
 

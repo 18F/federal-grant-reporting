1dbc0361163a704dda5bab5b9a426333e5e07405
Fureigh <rhys.fureigh@gsa.gov>
2019-08-29 13:38:42 -0700

    Temporarily set DEBUG to True in production
    This is *not* appropriate for an actual deployment, but it
    resolves the incompatibility between collectstatic and 'DEBUG=False'
    that happens with the current development server (i.e., runserver).
    
    For further context, see:
    https://docs.djangoproject.com/en/2.2/howto/static-files/
    
    https://github.com/18F/federal-grant-reporting/issues/87

--- single-audit/single_audit/settings/production.py
+++ single-audit/single_audit/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # DEBUG is currently only turned on in dev settings, but better safe than sorry.
-DEBUG = False
+DEBUG = True  # Revisit whether runserver is incompatible with collectstatic
 
 LOGGING = {
     'version': 1,

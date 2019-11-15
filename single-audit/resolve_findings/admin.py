cf05665df07ca0046b08c16121cc966a452d0e22
Joe Krzystan <joseph.krzystan@gsa.gov>
2019-11-15 10:53:52 -0500

    Remove ununsed User model

--- single-audit/resolve_findings/admin.py
+++ single-audit/resolve_findings/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Finding, Agency, Grant, Grantee, User, Comment
+from .models import Finding, Agency, Grant, Grantee, Comment
 
-admin.site.register(User, UserAdmin)
 admin.site.register(Agency)
 admin.site.register(Finding)
 admin.site.register(Grant)
